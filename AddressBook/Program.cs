b284bdf73201b8c704c4442eff31dcfb80722814
Arun37-Kumar <arun007624@gmail.com>
2022-04-27 20:04:33 +0530

    UC1 updated

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,6 +11,11 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book system!");
+
+            Contacts contact = new Contacts();
+            contact.firstName = "Arun";
+            Console.WriteLine("First name : "+contact.firstName);
+            Console.ReadLine();
         }
     }
 }
